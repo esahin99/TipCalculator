dff3130feef5ab160cd3a64a583884457f456e43
Emre Sahin <mehmetemre9906@gmail.com>
2023-03-08 23:22:48 +0300

    updated

---
+++ TipCalculator/Controllers/CalculatorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Emre Sahin on 8.03.2023.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+}
+
---
+++ TipCalculator/Controllers/ResultsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ResultsViewController.swift
+//  TipCalculator
+//
+//  Created by Emre Sahin on 8.03.2023.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}

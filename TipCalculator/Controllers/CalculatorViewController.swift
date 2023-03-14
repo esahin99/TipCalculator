536d154bf0f1a9047bae7ab875c979ad01fb9da5
Emre Sahin <mehmetemre9906@gmail.com>
2023-03-14 18:21:58 +0300

    updated

--- TipCalculator/Controllers/CalculatorViewController.swift
+++ TipCalculator/Controllers/CalculatorViewController.swift
@@ -15,14 +15,45 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-
+    var tip = 0.0
+    var numberOfPeople = 0
+    var bill = 0.0
+    
     @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        billTextField.endEditing(true)
+        
+        let buttonTitle = Double(sender.currentTitle!.dropLast())!
+        tip = buttonTitle/100
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        sender.isSelected = true
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        bill = Double(billTextField.text!)!
+        bill = bill + (bill*tip)
+        bill /= Double(numberOfPeople)
+        tip = tip*100
+        
+        performSegue(withIdentifier: "ResultVC", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ResultVC"{
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.tip = String(format: "%.0f", tip)
+            destinationVC.numberOfPeople = String(numberOfPeople)
+            destinationVC.results = String(format: "%.2f", bill)
+        }
     }
     
 }
--- TipCalculator/Controllers/ResultsViewController.swift
+++ TipCalculator/Controllers/ResultsViewController.swift
@@ -12,14 +12,19 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
+    var results: String?
+    var numberOfPeople: String?
+    var tip: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        totalLabel.text = results
+        settingsLabel.text = "Split between \(numberOfPeople ?? "Error") people, with \(tip ?? "Error")% tip"
         // Do any additional setup after loading the view.
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true)
     }
     
 }
